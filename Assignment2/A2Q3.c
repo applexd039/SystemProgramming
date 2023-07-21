e8940bafe3e79da33ad333b2ecc29a7d583655fa
applexd039 <105958031+applexd039@users.noreply.github.com>
2023-07-21 19:00:23 +0530

    Add files via upload

---
+++ Assignment2/A2Q3.c
@@ -0,0 +1,33 @@
+//accept directory name and print all file names from that directory
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<dirent.h>
+
+int main(int argc, char *argv[])
+{
+    DIR *dp = NULL;
+    struct dirent *entry = NULL;
+    char DirName[255] = {'\0'};
+
+    printf("Enter the directory name to open\n");
+    scanf("%s",DirName);
+
+    dp = opendir(DirName);
+    if(dp == NULL)
+    {
+        printf("Unable to open directory\n");
+        return -1;
+    }
+    printf("Name of files are :\n");
+
+    while((entry = readdir(dp)) != NULL)
+    {
+        printf("%s\n",entry->d_name);
+    }
+
+    closedir(dp);
+
+    return 0;
+}
