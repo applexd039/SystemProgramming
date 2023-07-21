79e9a9779e399370a549a0d656347fa2778c5072
applexd039 <105958031+applexd039@users.noreply.github.com>
2023-07-21 18:26:26 +0530

    Add files via upload

---
+++ Assignment1/A1Q1.c
@@ -0,0 +1,27 @@
+//accept file name and open that file
+#include<stdio.h>
+#include<fcntl.h>
+
+int main(int argc, char*argv[])
+{
+  int fd = 0;
+
+  if(argc != 2)
+  {
+    printf("Invalid number of arguments\n");
+    return -1;
+  }
+
+  fd = open(argv[1], O_RDONLY);
+  if(fd == -1)
+  {
+    printf("Unable to open file\n");
+    return -1;
+  }
+  else
+  {
+    printf("File is successfully opened with fd : %d\n",fd);
+  }
+
+  return 0;
+}
